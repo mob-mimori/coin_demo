d2d2a464fbe51aacdaa35527ff76ed7a5f6d85a2
hiroshi mimori <mimori@mobcast.jp>
2014-09-16 13:36:56 +0900

    init commit

---
+++ Assets/CoinEmitter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinEmitter : MonoBehaviour {
+
+	public GameObject medal;
+
+	public int hasCoin = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetButtonDown ("Fire1") && hasCoin > 0)
+        {
+        	Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast (ray, out hit, 100)){
+            	Vector3 v = hit.point + new Vector3(0, 1, 0);
+	            GameObject obj = Instantiate (medal, v, hit.transform.rotation) as GameObject;
+	            Debug.Log(hasCoin);
+	            hasCoin--;
+                //obj.transform.parent    =   manager.transform;
+
+            }
+        }
+	}
+}
---
+++ Assets/Slider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		iTween.MoveBy(gameObject, iTween.Hash("z", 2, "easeType", "easeInOutExpo", "loopType", "pingPong", "delay", .1));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
